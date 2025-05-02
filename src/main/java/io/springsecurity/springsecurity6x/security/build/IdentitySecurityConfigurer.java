9c9785f071b8b3f21bf4af8b1afe54ff0ebf89c9
onjsdnjs <onjsdnjs@gmail.com>
2025-05-02 23:30:24 +0900

    lecture

--- src/main/java/io/springsecurity/springsecurity6x/security/build/IdentitySecurityConfigurer.java
+++ src/main/java/io/springsecurity/springsecurity6x/security/build/IdentitySecurityConfigurer.java
@@ -4,7 +4,10 @@ import io.springsecurity.springsecurity6x.security.init.AuthenticationConfig;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 public interface IdentitySecurityConfigurer {
+
     boolean supports(AuthenticationConfig config);
+
     void configure(HttpSecurity http, AuthenticationConfig config) throws Exception;
+
     default int order() { return 0; }
 }

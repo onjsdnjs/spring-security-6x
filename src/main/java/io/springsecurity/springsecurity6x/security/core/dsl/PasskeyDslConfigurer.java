48790e4d8925ce7d8cf3b67c5bc2de248d93c3dd
onjsdnjs <onjsdnjs@gmail.com>
2025-05-03 20:10:38 +0900

    lecture

--- src/main/java/io/springsecurity/springsecurity6x/security/core/dsl/PasskeyDslConfigurer.java
+++ src/main/java/io/springsecurity/springsecurity6x/security/core/dsl/PasskeyDslConfigurer.java
@@ -1,12 +1,16 @@
 package io.springsecurity.springsecurity6x.security.core.dsl;
 
+import io.springsecurity.springsecurity6x.security.core.dsl.common.CommonSecurityDsl;
+import io.springsecurity.springsecurity6x.security.core.dsl.impl.OttDslConfigurerImpl;
+import io.springsecurity.springsecurity6x.security.core.dsl.impl.PasskeyDslConfigurerImpl;
+
 /**
  * Passkey(WebAuthn) 로그인 관련 DSL 설정을 정의하는 인터페이스입니다.
  * <p>
  * 사용자는 이 DSL을 통해 Passkey 인증 엔드포인트, Relying Party 정보, 허용된 출처 등을
  * 설정할 수 있으며, 이를 기반으로 플랫폼이 HttpSecurity에 Passkey 인증 필터를 적용합니다.
  */
-public interface PasskeyDslConfigurer {
+public interface PasskeyDslConfigurer extends CommonSecurityDsl<PasskeyDslConfigurerImpl> {
 
     PasskeyDslConfigurer matchers(String... patterns);
 
--- src/main/java/io/springsecurity/springsecurity6x/security/core/dsl/RestDslConfigurer.java
+++ src/main/java/io/springsecurity/springsecurity6x/security/core/dsl/RestDslConfigurer.java
@@ -1,12 +1,16 @@
 package io.springsecurity.springsecurity6x.security.core.dsl;
 
+import io.springsecurity.springsecurity6x.security.core.dsl.common.CommonSecurityDsl;
+import io.springsecurity.springsecurity6x.security.core.dsl.impl.PasskeyDslConfigurerImpl;
+import io.springsecurity.springsecurity6x.security.core.dsl.impl.RestDslConfigurerImpl;
+
 /**
  * REST 로그인 관련 DSL 설정을 정의하는 인터페이스입니다.
  * <p>
  * 사용자는 이 DSL을 통해 REST 로그인 처리 엔드포인트와 매처를 설정하고,
  * 이를 기반으로 플랫폼이 HttpSecurity에 REST 인증 필터를 적용합니다.
  */
-public interface RestDslConfigurer {
+public interface RestDslConfigurer extends CommonSecurityDsl<RestDslConfigurerImpl> {
 
     RestDslConfigurer matchers(String... patterns);
 

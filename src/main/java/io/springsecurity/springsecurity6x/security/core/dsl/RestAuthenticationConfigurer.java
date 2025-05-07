07600e42f48146b364814750d4bae370ab6fe1e7
onjsdnjs <onjsdnjs@gmail.com>
2025-05-07 17:11:38 +0900

    lecture

--- src/main/java/io/springsecurity/springsecurity6x/security/core/dsl/RestAuthenticationConfigurer.java
+++ src/main/java/io/springsecurity/springsecurity6x/security/core/dsl/RestAuthenticationConfigurer.java
@@ -1,25 +1,77 @@
 package io.springsecurity.springsecurity6x.security.core.dsl;
 
 import io.springsecurity.springsecurity6x.security.filter.RestAuthenticationFilter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.ott.OneTimeTokenLoginConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.Assert;
 
-public final class RestAuthenticationConfigurer<H extends HttpSecurityBuilder<H>> extends
-        AbstractAuthenticationFilterConfigurer<H, RestAuthenticationConfigurer<H>, RestAuthenticationFilter> {
+public final class RestAuthenticationConfigurer<H extends HttpSecurityBuilder<H>>
+        extends AbstractHttpConfigurer<RestAuthenticationConfigurer<H>, H> {
+
+    private String loginProcessingUrl = "/api/auth/login";
+    private RequestMatcher requestMatcher = new AntPathRequestMatcher(loginProcessingUrl, HttpMethod.POST.name());
+    private AuthenticationSuccessHandler successHandler;
+    private AuthenticationFailureHandler failureHandler;
+    private SecurityContextRepository securityContextRepository;
 
     public RestAuthenticationConfigurer() {
-        super(new RestAuthenticationFilter(), null);
     }
 
     @Override
-    public RestAuthenticationConfigurer<H> loginPage(String loginPage) {
-        return super.loginPage(loginPage);
+    public void configure(H http) throws Exception {
+        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+        Assert.notNull(authenticationManager, "AuthenticationManager is required");
+
+        RestAuthenticationFilter restFilter = new RestAuthenticationFilter(authenticationManager);
+        restFilter.setRequestMatcher(requestMatcher);
+
+        if (successHandler != null) {
+            restFilter.setSuccessHandler(successHandler);
+        }
+        if (failureHandler != null) {
+            restFilter.setFailureHandler(failureHandler);
+        }
+        if (securityContextRepository != null) {
+            restFilter.setSecurityContextRepository(securityContextRepository);
+        } else {
+            restFilter.setSecurityContextRepository(new RequestAttributeSecurityContextRepository());
+        }
+        restFilter.setSecurityContextHolderStrategy(SecurityContextHolder.getContextHolderStrategy());
+
+        http.addFilter(postProcess(restFilter));
     }
 
-    @Override
-    protected RequestMatcher createLoginProcessingUrlMatcher(String loginProcessingUrl) {
-        return new AntPathRequestMatcher(loginProcessingUrl, "POST");
+    public RestAuthenticationConfigurer<H> loginProcessingUrl(String loginProcessingUrl) {
+        Assert.hasText(loginProcessingUrl, "loginProcessingUrl must not be null or empty");
+        this.loginProcessingUrl = loginProcessingUrl;
+        this.requestMatcher = new AntPathRequestMatcher(this.loginProcessingUrl, HttpMethod.POST.name());
+        return this;
+    }
+
+    public RestAuthenticationConfigurer<H> successHandler(AuthenticationSuccessHandler successHandler) {
+        this.successHandler = successHandler;
+        return this;
+    }
+
+    public RestAuthenticationConfigurer<H> failureHandler(AuthenticationFailureHandler failureHandler) {
+        this.failureHandler = failureHandler;
+        return this;
+    }
+
+    public RestAuthenticationConfigurer<H> securityContextRepository(SecurityContextRepository repository) {
+        this.securityContextRepository = repository;
+        return this;
     }
 }

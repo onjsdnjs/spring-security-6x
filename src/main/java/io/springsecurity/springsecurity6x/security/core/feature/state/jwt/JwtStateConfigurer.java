5d84e1a60c34d96b8d97689a3441c40d2db7cc5f
onjsdnjs <onjsdnjs@gmail.com>
2025-05-08 22:46:50 +0900

    lecture

--- src/main/java/io/springsecurity/springsecurity6x/security/core/feature/state/jwt/JwtStateConfigurer.java
+++ src/main/java/io/springsecurity/springsecurity6x/security/core/feature/state/jwt/JwtStateConfigurer.java
@@ -4,16 +4,11 @@ import io.springsecurity.springsecurity6x.security.filter.JwtAuthorizationFilter
 import io.springsecurity.springsecurity6x.security.filter.JwtPreAuthenticationFilter;
 import io.springsecurity.springsecurity6x.security.filter.JwtRefreshAuthenticationFilter;
 import io.springsecurity.springsecurity6x.security.token.service.TokenService;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.function.Supplier;
 
@@ -21,29 +16,6 @@ import java.util.function.Supplier;
  * JWT 상태 전략을 HttpSecurity에 적용하는 설정자
  */
 public class JwtStateConfigurer extends AbstractHttpConfigurer<JwtStateConfigurer, HttpSecurity> {
-
-    @Override
-    public void init(HttpSecurity http) throws Exception {
-
-        Supplier<LogoutHandler> logoutSupplier = () -> http.getSharedObject(LogoutHandler.class);
-
-        /*LogoutConfigurer<HttpSecurity> logout = http.getConfigurer(LogoutConfigurer.class);
-        if (logout == null) {
-            http.with(new LogoutConfigurer<>(), Customizer.withDefaults());
-        }
-
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .exceptionHandling(e -> e
-                        .authenticationEntryPoint((req,res,ex) -> res.sendError(401))
-                        .accessDeniedHandler((req,res,ex) -> res.sendError(403)))
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .logout(l -> l
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/api/auth/logout"))
-                        .addLogoutHandler(logoutSupplier.get())
-                        .logoutSuccessHandler(http.getSharedObject(LogoutSuccessHandler.class))
-                );*/
-    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {

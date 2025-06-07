fcfa4d6e1b5e495a1ac5515bad709f71d8866b42
onjsdnjs <onjsdnjs@gmail.com>
2025-06-07 23:24:02 +0900

    initial

--- src/main/java/io/springsecurity/springsecurity6x/security/authz/context/DefaultContextHandler.java
+++ src/main/java/io/springsecurity/springsecurity6x/security/authz/context/DefaultContextHandler.java
@@ -1,19 +1,16 @@
 package io.springsecurity.springsecurity6x.security.authz.context;
 
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
 @Component
-@RequiredArgsConstructor
 public class DefaultContextHandler implements ContextHandler {
-
     @Override
     public AuthorizationContext create(Authentication authentication, HttpServletRequest request) {
         ResourceDetails resource = new ResourceDetails("URL", request.getRequestURI());
-        EnvironmentDetails environment = new EnvironmentDetails(request.getRemoteAddr(), LocalDateTime.now());
+        EnvironmentDetails environment = new EnvironmentDetails(request.getRemoteAddr(), LocalDateTime.now(), request);
 
         return new AuthorizationContext(authentication, resource, request.getMethod(), environment);
     }
--- src/main/java/io/springsecurity/springsecurity6x/security/authz/context/EnvironmentDetails.java
+++ src/main/java/io/springsecurity/springsecurity6x/security/authz/context/EnvironmentDetails.java
@@ -1,10 +1,13 @@
 package io.springsecurity.springsecurity6x.security.authz.context;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 /**
- * 요청 환경(Environment)의 상세 정보를 담는 객체.
+ * 요청 환경(Environment)의 상세 정보를 담는 Record.
  */
 public record EnvironmentDetails(
         String remoteIp,
-        LocalDateTime timestamp) {}
+        LocalDateTime timestamp,
+        HttpServletRequest request // 원본 요청 객체에 대한 참조를 유지하여 확장성 확보
+) {}
